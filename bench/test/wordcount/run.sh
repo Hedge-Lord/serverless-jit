f9d418b90a17ba06b7cfcf11eece8b9202859566
Hedge-Lord <georgezyst@gmail.com>
2025-11-12 01:46:21 -0800

    wordcount test

--- bench/test/wordcount/run.sh
+++ bench/test/wordcount/run.sh
@@ -1,11 +1,11 @@
-JAVA=/Users/georgezhou/personal/research/serverless-jit/jdk25u/build/macosx-aarch64-server-release/jdk/bin/java
+set -euo pipefail
 
-rm /tmp/mdo.log
+JAVA=/Users/georgezhou/personal/research/serverless-jit/jdk25u/build/macosx-aarch64-server-release/images/jdk/bin/java
 
-echo "Running MDO Dump"
-$JAVA  -XX:+UnlockDiagnosticVMOptions -XX:+DumpMDOAtExit -XX:MDOReplayDumpFile=/tmp/mdo.log -Xlog:compilation=info WordCountBenchmark.java 0 1000 cold.csv resource.txt 
+rm -f /tmp/wc_mdo.bin
 
-echo "Running Baseline"
-$JAVA   WordCountBenchmark.java 0 1000 cold.csv resource.txt 
-echo "Running Warm"
-$JAVA -XX:+UnlockDiagnosticVMOptions -XX:+LoadMDOAtStartup -XX:MDOReplayLoadFile=/tmp/mdo.log -Xlog:compilation=info WordCountBenchmark.java 0 1000 warm.csv resource.txt
+echo "Running cold dump"
+$JAVA -XX:+UnlockDiagnosticVMOptions -XX:+DumpMDOAtExit -XX:MDOReplayDumpFile=/tmp/wc_mdo.bin -Xlog:jit+compilation=debug WordCountBenchmark 0 1000 out/cold_dump.csv resource.txt > ./out/cold_dump.log 2>&1
+
+echo "Running warm load"
+$JAVA -XX:+UnlockDiagnosticVMOptions -XX:+LoadMDOAtStartup -XX:MDOReplayLoadFile=/tmp/wc_mdo.bin -Xlog:jit+compilation=debug WordCountBenchmark 0 1000 out/warm_load.csv resource.txt > ./out/warm_load.log 2>&1
